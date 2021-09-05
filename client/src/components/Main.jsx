e250d30253627cc73e1d695abb257938af428527
Ryan-Ack <ryan.ack0410@gmail.com>
2021-09-05 15:02:14 -0700

    Back end working, adding front end components ra

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link } from '@reach/router'
+
+const Main = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Main
